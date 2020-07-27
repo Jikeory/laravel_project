5b9478aa3f7074fe20f9d9deadbb7a1907178017
jikeory.song <jikeory.song@ibaiqiu.com>
2020-07-27 17:24:06 +0800

    rebase测试

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,6 @@ use Illuminate\Support\Facades\Route;
 // });
 // Route::get('/', 'AdminHomeController@index');
 Route::group(['middleware' => ['auth', 'XSS']], function () {
-    Route::get('login', 'Auth\LoginController@login');
+    // Route::get('login', 'Auth\LoginController@login');
     Route::get('/', 'AdminHomeController@index');
 });
